dcbc2a7b5566299c4ce515de5cf578a9e9a73a9c
KaplanDS3 <oriorigeva12@gmail.com>
2023-12-17 12:48:23 +0200

    The adventure begins!

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from kivy import *
-from kivy.app import *
-from kivy.uix.label import *
-from kivy.uix.button import *
-
-
-class Main(App):
-    def build(self):
-        return Button(text="shalom 2", size_hint=(0.5, 0.5))
-
-
-Main().run()
+import kivy
