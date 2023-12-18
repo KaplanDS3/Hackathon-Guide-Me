a4fd3ca9e99333a2d5c4bf56f69effb23d31add7
ompa07 <rina.froen@gmail.com>
2023-12-18 16:32:21 +0200

    הוספתי מסך לכשאין אזעקה

--- no_alert.py
+++ no_alert.py
@@ -1,33 +1,20 @@
+from kivy.graphics import Rectangle
+from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle, Color
 from kivy.core.window import Window
 
 Window.size = (350, 600)
 
 
-class FirstScreen(Widget):
-
+class Background(BoxLayout):
     def __init__(self, **kwargs):
-        super(FirstScreen, self).__init__(**kwargs)
-
-        with self.canvas:
-            #Color(0, 0, 0, 1)
-
-            # Setting the size and position of canvas
-            self.rect = Rectangle(source="no_alert.png",
-                                  pos=self.center, size=self.size)
-
-            # Update the canvas as the screen size change
-            self.bind(pos=self.update_rect,
-                      size=self.update_rect)
-
-    def update_rect(self, *args):
-        self.rect.pos = self.pos
-        self.rect.size = self.size
+        super().__init__(**kwargs)
 
 
 class no_alert(App):
     def build(self):
-        return FirstScreen()
+        return Background()
 
+
+if __name__ == '__main__':
+    no_alert().run()
