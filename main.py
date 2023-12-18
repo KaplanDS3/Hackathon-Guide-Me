e9480c1d71366534b27199fa436ccbb52a7c3cb0
teaSun1 <154055259+teaSun1@users.noreply.github.com>
2023-12-18 15:10:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle, Color
-import no_boom_shelter
-import yes_boom_shelter
-import no_alert
-from kivy.core.window import Window
-
-Window.size = (400, 700)
+from gtts import gTTS
+import os
 
 
-class Main():
-    alarm = False
-    hasBoomShelter = False
-    if not alarm:
-        no_alert.no_alert().run()
-    else:
-        if hasBoomShelter:
-            yes_boom_shelter.yes_boom_shelter().run()
-        else:
-            no_boom_shelter.noBoomShelter().run()
+def read_file_out_loud(file_path):
+    try:
+        # Read the content of the file
+        with open(file_path, 'r') as file:
+            file_content = file.read()
 
+        # Create a gTTS object
+        tts = gTTS(text=file_content, lang='en')
 
+        # Save the audio file
+        audio_file_path = 'output.mp3'
+        tts.save(audio_file_path)
+
+        # Play the audio file using the default audio player
+        os.system(f'start {audio_file_path}')
+
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
+def red_alart_area():
+    file_path = 'red_alart'
+    read_file_out_loud(file_path)
+
+def no_red_alart():
+    file_path = 'no_red_alart'
+    read_file_out_loud(file_path)
+
+def no_shelter_around():
+    file_path = 'no_shelter_around_txt'
+    read_file_out_loud(file_path)
 if __name__ == '__main__':
-    Main()
+    # Replace 'your_file.txt' with the path to your text file
+    file_path = 'no_shelter_around_txt'
+    read_file_out_loud(file_path)
+
